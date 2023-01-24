0e4b3b3eba769063ae68b5c7a2a0b4e1590f5bff
arberk6 <arkadriu@gmail.com>
2023-01-24 01:41:53 +0100

    update

---
+++ src/Page1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Page1 = () => {
+    return <>
+        <img
+            src="https://cdn.pixabay.com/photo/2014/06/03/19/38/board-361516__340.jpg"
+        />
+        <h2>21 characters</h2>
+    </>
+}
